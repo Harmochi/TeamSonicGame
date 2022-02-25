7a6948cb3f99bd257a8ff775f4040a7705ca4eb1
hollyiscool <hsims2@stu.chi.ac.uk>
2022-02-25 07:30:58 +0000

    Started on Animations, not working

---
+++ Assets/Script/PlayerAttack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayer;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        //Play Attack Animation
+        animator.SetTrigger("Player_Attacking");
+
+        //Detect Enemies
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+        //Damage Enemies
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log(" We hit " + enemy.name);
+        }
+    }
+}
