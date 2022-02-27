beb12d68c8e5874702c5bcf6e8a4cc35da60a183
hollyiscool <hsims2@stu.chi.ac.uk>
2022-02-27 17:47:41 +0000

    Set up beginning of level 1. adjusted attack so enemies only get fit with knife instead of the whole player

--- Assets/Script/EnemyHealth.cs
+++ Assets/Script/EnemyHealth.cs
@@ -21,7 +21,7 @@ public class EnemyHealth : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == ("Player") && !hasEntered)
+        if (collision.gameObject.tag == ("AttackPoint") && !hasEntered)
         {
             TakeDamage(1);
         }
