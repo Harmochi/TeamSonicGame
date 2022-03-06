336f10eba1f0edb4981a94be2bf92c0360a0213b
hollyiscool <hsims2@stu.chi.ac.uk>
2022-03-06 10:40:58 +0000

    so many spikes owie ouch

---
+++ Assets/Script/Sword/SwordMovementOld.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordMovementOld : MonoBehaviour
+{
+    [SerializeField] private GameObject magnet;
+    [SerializeField] private float speed = 1.5f;
+
+    void Start()
+    {
+        GetComponent<SwordPlatformOld>().enabled = false;
+    }
+
+    void Update()
+    {
+
+        //activate other script
+        if (Input.GetMouseButtonDown(0)) transform.eulerAngles = new Vector3(0, 0, 270);
+        if (Input.GetMouseButtonDown(0)) GetComponent<SwordPlatformOld>().enabled = true;
+        if (Input.GetMouseButtonDown(0)) GetComponent<SwordMovementOld>().enabled = false;
+
+        //Box collider
+        GetComponent<Collider2D>().enabled = false;
+
+        //Following
+        transform.position = Vector2.MoveTowards(transform.position, magnet.transform.position, speed * Time.deltaTime);
+    }
+}
